3e0d516d05d7381409822335cefa51620daa4587
esraa alsharit <esraa.alsharit@gmail.com>
2022-07-20 15:24:58 +0300

    Death is the only way

---
+++ Death is the only way/Library/Il2cppBuildCache/WebGL/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,115 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void About::StartGame()
+extern void About_StartGame_m40F9EAEB61664E61D2880D828BFA60234C081DEA (void);
+// 0x00000002 System.Void About::.ctor()
+extern void About__ctor_m3FEC3A3E06296EA3AE8A0C7D16C83AF2331F7FB1 (void);
+// 0x00000003 System.Void Back::StartGame()
+extern void Back_StartGame_mFEFE286C884937A8C90AC4ACE95283FC5C7BDE5D (void);
+// 0x00000004 System.Void Back::.ctor()
+extern void Back__ctor_m9CFE057B8016D16F243B994A0B54F327947A052B (void);
+// 0x00000005 System.Void CameraController::Start()
+extern void CameraController_Start_mBDE87C2FCF352957C2B86B67610667663422FBE6 (void);
+// 0x00000006 System.Void CameraController::Update()
+extern void CameraController_Update_m3C257AC762117CFDDAD03C9C4FBBFDE51C61D534 (void);
+// 0x00000007 System.Void CameraController::.ctor()
+extern void CameraController__ctor_m07EC5A8C82742876097619BE7DD9043F47327DAE (void);
+// 0x00000008 System.Void Death::.ctor()
+extern void Death__ctor_mDC0979D381A51CB62F77EE5082E9DBFD6D9E86C1 (void);
+// 0x00000009 System.Void End::Quit()
+extern void End_Quit_m4294F7CDDFB3D0A1980587C1DE295B149B31A9D3 (void);
+// 0x0000000A System.Void End::.ctor()
+extern void End__ctor_m205CBA8A3445D0A0E1224ACFD8A032DF066876DF (void);
+// 0x0000000B System.Void Player::Start()
+extern void Player_Start_mBD692B64AAC791B93A589E7D3596F36787EAF021 (void);
+// 0x0000000C System.Void Player::Update()
+extern void Player_Update_mBA04F1D6FE3C18037EA95DFAEEAA9977BFD49CD3 (void);
+// 0x0000000D System.Void Player::OnTriggerEnter2D(UnityEngine.Collider2D)
+extern void Player_OnTriggerEnter2D_mAF357F7244427CB9EADB81B5A6C4F0AF481641D0 (void);
+// 0x0000000E System.Void Player::.ctor()
+extern void Player__ctor_mDE8EB5B351975D4E7E24DE341B8B49B8A29CC2B7 (void);
+// 0x0000000F System.Void Restart::StartGame()
+extern void Restart_StartGame_m11C229DBD80FC8D96D0CF1F1C7D15EA3F17E32E3 (void);
+// 0x00000010 System.Void Restart::.ctor()
+extern void Restart__ctor_mC040D6C84CCAA3DF8BE8CDBF1C030FBAA1331FAE (void);
+// 0x00000011 System.Void Start::StartGame()
+extern void Start_StartGame_m8F202992C362DC7EF3F593711B278CA928326418 (void);
+// 0x00000012 System.Void Start::.ctor()
+extern void Start__ctor_mEDE333A1BC25ACBF1367AB25456191E96822BF58 (void);
+static Il2CppMethodPointer s_methodPointers[18] = 
+{
+	About_StartGame_m40F9EAEB61664E61D2880D828BFA60234C081DEA,
+	About__ctor_m3FEC3A3E06296EA3AE8A0C7D16C83AF2331F7FB1,
+	Back_StartGame_mFEFE286C884937A8C90AC4ACE95283FC5C7BDE5D,
+	Back__ctor_m9CFE057B8016D16F243B994A0B54F327947A052B,
+	CameraController_Start_mBDE87C2FCF352957C2B86B67610667663422FBE6,
+	CameraController_Update_m3C257AC762117CFDDAD03C9C4FBBFDE51C61D534,
+	CameraController__ctor_m07EC5A8C82742876097619BE7DD9043F47327DAE,
+	Death__ctor_mDC0979D381A51CB62F77EE5082E9DBFD6D9E86C1,
+	End_Quit_m4294F7CDDFB3D0A1980587C1DE295B149B31A9D3,
+	End__ctor_m205CBA8A3445D0A0E1224ACFD8A032DF066876DF,
+	Player_Start_mBD692B64AAC791B93A589E7D3596F36787EAF021,
+	Player_Update_mBA04F1D6FE3C18037EA95DFAEEAA9977BFD49CD3,
+	Player_OnTriggerEnter2D_mAF357F7244427CB9EADB81B5A6C4F0AF481641D0,
+	Player__ctor_mDE8EB5B351975D4E7E24DE341B8B49B8A29CC2B7,
+	Restart_StartGame_m11C229DBD80FC8D96D0CF1F1C7D15EA3F17E32E3,
+	Restart__ctor_mC040D6C84CCAA3DF8BE8CDBF1C030FBAA1331FAE,
+	Start_StartGame_m8F202992C362DC7EF3F593711B278CA928326418,
+	Start__ctor_mEDE333A1BC25ACBF1367AB25456191E96822BF58,
+};
+static const int32_t s_InvokerIndices[18] = 
+{
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+	956,
+	1114,
+	1114,
+	1114,
+	1114,
+	1114,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	18,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
